3e588dbb387b3535995e93173cba7dfa2f5e56f3
shravaniii29 <sbhole295@gmail.com>
2026-06-25 17:11:51 +0530

    More Array LC Questions

---
+++ Arrays/Easy/Pascals_Triangle_2.py
@@ -0,0 +1,21 @@
+from typing import List
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[int]:
+
+        row = [1]
+
+        for i in range(1, rowIndex + 1):
+
+            newRow = [1] * (i + 1)
+
+            for j in range(1, i):
+                newRow[j] = row[j - 1] + row[j]
+
+            row = newRow
+
+        return row
+
+
+# Time Complexity: O(n²)
+# Space Complexity: O(n)
