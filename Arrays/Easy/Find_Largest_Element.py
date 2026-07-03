f7fb427a69b23d0b2c7387c87916c88b6052735e
shravaniii29 <sbhole295@gmail.com>
2026-07-03 16:45:37 +0530

    Add comments for largest element initialization logic
    Added comments to clarify the initial assumption for the largest element.

--- Arrays/Easy/Find_Largest_Element.py
+++ Arrays/Easy/Find_Largest_Element.py
@@ -1,3 +1,6 @@
+# Here we have taken the first array element as our largest element initially,
+# but if it is mentioned that the array contains negative elements as well, then take largest element (maxi here) as -infinity
+
 def find_max(arr):
     maxi = arr[0]
 
